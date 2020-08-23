cb7d6c0c0618bebb29abfd76e7d5a57a8a2a398e
krtvngtjnr <krtvngtjnr@gmail.com>
2020-08-23 19:20:17 +0900

    Angularフロントエンド側作成

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from './product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+  {
+    path: 'products', component: ProductComponent,
+    children: [
+      { path: '', component: ProductListComponent },
+      { path: ':productId', component: ProductDetailComponent },
+    ]
+  }
+];
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    ProductDetailComponent,
+    ProductListComponent
+  ],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [],
+})
+export class ProductModule { }
